6d8357b5b5c11dfb39a3f34e88114e2f1efeb620
streed <sean@cheapfit.me>
2014-01-28 00:06:15 -0500

    Working on making this class a singleton

--- simpleGossip/gossiping/gossip.py
+++ simpleGossip/gossiping/gossip.py
@@ -3,8 +3,8 @@ from ..mailbox.router import Router
 
 class Gossip( object ):
 
-    def __init__( self, seed=[], fanout=10 )
-        self.router = Router()
+    def __init__( self, router=None seed=[], fanout=10 )
+        self.router = router
         self.total_nodes = 0
         self.total_letters_sent = 0
         self.total_letters_received = 0
@@ -12,3 +12,31 @@ class Gossip( object ):
         self.total_unhealthy_nodes = 0
 
         self.system = Mailbox( "system" )
+
+        self.neighborhood = seed
+        self.fanout = fanout
+
+        if( self.router ):
+            self.router.add_route( "new_node", self.new_node )
+            self.router.add_route( "dead_nodea", self.dead_node )
+            self.router.add_route( "view", self.view )
+
+
+    def new_node( self, letter ):
+        return letter
+
+    def dead_node( self, letter ):
+        return letter
+
+    def view( self, letter ):
+        return letter
+
+    def to_dict( self ):
+        return dict(
+                    fanout=self.fanout,
+                    neighborhood=self.neighborhood,
+                    total_nodes=self.total_nodes,
+                    total_letters_sent=self.total_letters_sent,
+                    total_letters_received=self.total_letters_received,
+                    total_healthy_nodes=self.total_healthy_nodes,
+                    total_unhealthy_nodes=self.total_unhealthy_nodes )
